17bcfcc1204b3581748fff029ef8df308e7f3d31
Mohamad Farid Saleh <github.farid1@gmail.com>
2023-12-21 15:07:36 +0700

    update scripts

--- pc_scraper.py
+++ pc_scraper.py
@@ -1,28 +1,22 @@
 import feedparser
 from datetime import datetime
 from dateutil import parser as dparser
-
 import humanize
 import time
 import requests
-import os
 from dotenv import load_dotenv
-load_dotenv()
+import os
+from playsound import playsound
 
-# post_age = 600 #10 minutes
+load_dotenv()
 old_entries = []
-TOKEN = os.getenv("TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-MESSAGE = "RSS reader every 60 seconds"
 ORGID=os.getenv("ORGID")
 SECURITY_TOKEN=os.getenv("SECURITY_TOKEN")
 TOPIC=os.getenv("TOPIC")
 USER_ID=os.getenv("USER_ID")
 POST_AGE=os.getenv("POST_AGE")
-
-telegram_url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={MESSAGE}"
-requests.get(telegram_url)
 while True:
+    #web scraper
     rssurl = f"https://www.upwork.com/ab/feed/topics/rss?securityToken={SECURITY_TOKEN}&userUid={USER_ID}&orgUid={ORGID}&topic={TOPIC}"
     NewsFeed = feedparser.parse(rssurl)
     now = datetime.utcnow().replace(second=0, microsecond=0)
@@ -39,8 +33,6 @@ while True:
                 diff_human = humanize.naturaltime(diff)
                 message = f"{title}\n {link}\n{diff_human}"
                 old_entries.append(feed.id)
-                telegram_url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}"
-                r = requests.get(telegram_url)
-                print(r)
-                old_entries.append(feed.id)
+                print(message)
+                playsound('sound1.wav')
     time.sleep(60)
